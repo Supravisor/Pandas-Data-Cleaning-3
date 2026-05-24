a371c6e5f3d551c884af71dba42e4950da74ab69
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-05-25 09:21:21 +1200

    add readContents function

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 // Reading data with Python
 let filePath = document.getElementById("filePath");
 let fileObject = document.getElementById("fileObject");
+
+const readContents = () => {
+  if (filePath.value === "") {
+    return alert("Please enter a file path in the 'file path' field, in the 'Reading data with Python' section.");
+  } else if (fileObject.value === "") {
+      return alert("Please enter a file object in the 'file object' field, in the 'Reading data with Python' section.");
+  } else {
+      document.editor.textbox.value+="\nwith open('" + filePath.value  + ".csv', 'r') as " + fileObject.value + ":\n    for index, line in enumerate(" + fileObject.value + ".readlines()):\n        if (index < 10):\n            print(index, line)";
+  }
+}
