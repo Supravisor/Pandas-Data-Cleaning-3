d4ffa3cd1dae4195aa01ef3c10276867acdbde3c
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-06-10 13:36:48 +1200

    add parseDates variable

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ let naValues = document.editor.naValues;
 let sep = document.editor.sep;
 let decimal = document.editor.decimal;
 let thousands = document.editor.thousands;
+let parseDates = document.editor.parseDates;
 
 const readCsv = () => {
   if (filePath.value === "") {
